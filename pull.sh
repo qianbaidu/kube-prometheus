3f3cec0846ec4850178a783f4bc956459790c563
local-k8s-master <vbox-k8s-master@example.com>
2020-04-27 01:08:06 +0800

    replace docker images url

---
+++ pull.sh
@@ -0,0 +1,12 @@
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:metrics-server-amd64v0.2.0
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:k8s-prometheus-adapter-amd64v0.5.0
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:kube-rbac-proxyv0.4.1
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:kube-state-metricsv1.9.5
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:prometheus-operatorv0.38.1
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:prometheus_demo_service
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:alertmanagerv0.20.0
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:node-exporterv0.18.1
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:prometheusv2.15.2
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:prometheus-config-reloaderv0.38.1
+sudo docker pull  registry.cn-beijing.aliyuncs.com/hgliu/kube-prometheus:grafanav6.6.0
+
